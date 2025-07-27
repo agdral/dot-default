9b887df236254393255dddbf3065c32d86d25292
argonarch <armando12123434@gmail.com>
2025-07-27 18:05:05 -0300

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dotfile Default";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      system = system;
+    };
+  in {
+    nixosModules.default = import ./default.nix;
+    packages.${system}.default = pkgs.writeText "dummy" "Nixvim Config: only módules";
+  };
+}
