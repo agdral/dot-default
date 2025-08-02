1f22f1ab7839b8bb8404850e011ee714aa0718f7
argonarch <armando12123434@gmail.com>
2025-08-02 01:07:49 -0300

    minimal flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,12 @@
 {
   description = "Dotfile Default";
 
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      system = system;
+  outputs = {...}: {
+    nixosModules.default = {...}: {
+      imports = [
+        ./packages.nix
+        ./services.nix
+      ];
     };
-  in {
-    nixosModules.default = import ./default.nix;
-    packages.${system}.default = pkgs.writeText "dummy" "Nixvim Config: only módules";
   };
 }
